3b70ed67a706a41a0fe32085bda7e9a154698f71
m <mikkelelvers@outlook.com>
2025-04-02 13:52:46 +0200

    added an image to the main screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import { HydrateClient } from "@/trpc/server";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <HydrateClient>
-      <main className="w-screen h-screen p-4 md:p-8 text-neutral-900"></main>
+      <main className="w-screen h-screen text-neutral-900">
+        <Image
+          src={"/vinyl_background.jpg"}
+          alt="vinyl background"
+          width={1000}
+          height={1000}
+          className="w-full h-1/2 md:h-full object-cover"
+        />
+      </main>
     </HydrateClient>
   )
 }
